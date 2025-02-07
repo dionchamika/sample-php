bec5015c436507d7eec471513ba26d7cdf1ba40a
Dion Chamika <129253554+dionchamika@users.noreply.github.com>
2025-02-07 22:09:22 +0530

    commit

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 $host = "slt-clothing-db.c18qswy2ixqk.us-east-1.rds.amazonaws.com"; // Update with actual RDS endpoint
 $username = "admin";
 $password = "sltclothing12345";
-$database = "slt_clothing_db"; // Your actual database name
+$database = "slt_clothing"; // Your actual database name
 
 $conn = new mysqli($host, $username, $password, $database);
 
